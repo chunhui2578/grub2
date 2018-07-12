0985a3fd2226baab63a508c21e5d1e4a828f524d
Peter Jones <pjones@redhat.com>
2018-07-12 11:00:45 -0400

    Rework how the fdt command builds.
    Trying to avoid all variants of:
    cat syminfo.lst | sort | gawk -f ../../grub-core/genmoddep.awk > moddep.lst || (rm -f moddep.lst; exit 1)
    grub_fdt_install in linux is not defined
    grub_fdt_load in linux is not defined
    grub_fdt_unload in linux is not defined
    grub_fdt_install in xen_boot is not defined
    grub_fdt_load in xen_boot is not defined
    grub_fdt_unload in xen_boot is not defined
    
    This is an egregious hack until I can figure out how linking this thing is
    supposed to work.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grub-core/loader/efi/fdt.c
+++ grub-core/loader/efi/fdt.c
@@ -20,11 +20,16 @@
 #include <grub/mm.h>
 #include <grub/err.h>
 #include <grub/dl.h>
+#include <grub/misc.h>
 #include <grub/command.h>
 #include <grub/file.h>
 #include <grub/efi/efi.h>
 #include <grub/efi/fdtload.h>
 #include <grub/efi/memory.h>
+
+#include "../../lib/fdt.c"
+
+GRUB_MOD_LICENSE ("GPLv3+");
 
 static void *loaded_fdt;
 static void *fdt;
@@ -34,6 +39,28 @@ static void *fdt;
 #define FDT_ADDR_SIZE_EXTRA ((2 * grub_fdt_prop_entry_size (sizeof(grub_uint32_t))) + \
                              sizeof (FDT_ADDR_CELLS_STRING) + \
                              sizeof (FDT_SIZE_CELLS_STRING))
+
+static void *
+grub_efi_get_firmware_fdt (void)
+{
+  grub_efi_configuration_table_t *tables;
+  grub_efi_guid_t fdt_guid = GRUB_EFI_DEVICE_TREE_GUID;
+  void *firmware_fdt = NULL;
+  unsigned int i;
+
+  /* Look for FDT in UEFI config tables. */
+  tables = grub_efi_system_table->configuration_table;
+
+  for (i = 0; i < grub_efi_system_table->num_table_entries; i++)
+    if (grub_memcmp (&tables[i].vendor_guid, &fdt_guid, sizeof (fdt_guid)) == 0)
+      {
+	firmware_fdt = tables[i].vendor_table;
+	grub_dprintf ("linux", "found registered FDT @ %p\n", firmware_fdt);
+	break;
+      }
+
+  return firmware_fdt;
+}
 
 void *
 grub_fdt_load (grub_size_t additional_size)
